99da3957b9d0c3aa789db18bdf898f7fc98b07bc
Peter Daniel <petrus.daniel@protonmail.com>
2022-03-14 14:33:12 +0100

    Add collision detection.

--- js/TileMap.js
+++ js/TileMap.js
@@ -119,4 +119,55 @@ export default class TileMap {
       }
     }
   }
+
+  setCanvasSize(canvas) {
+    canvas.width = this.gameMap[0].length * this.tileSize;
+    canvas.height = this.gameMap.length * this.tileSize;
+  }
+
+  didCollideWithEnvironment(x, y, direction) {
+    if (direction == null) {
+      return;
+    }
+
+    if (
+      Number.isInteger(x / this.tileSize) &&
+      Number.isInteger(y / this.tileSize)
+    ) {
+      let column = 0;
+      let row = 0;
+      let nextColumn = 0;
+      let nextRow = 0;
+
+      switch (direction) {
+        case MovingDirection.right:
+          nextColumn = x + this.tileSize;
+          column = nextColumn / this.tileSize;
+          row = y / this.tileSize;
+          break;
+        case MovingDirection.left:
+          nextColumn = x - this.tileSize;
+          column = nextColumn / this.tileSize;
+          row = y / this.tileSize;
+          break;
+        case MovingDirection.up:
+          nextRow = y - this.tileSize;
+          row = nextRow / this.tileSize;
+          column = x / this.tileSize;
+          break;
+        case MovingDirection.down:
+          nextRow = y + this.tileSize;
+          row = nextRow / this.tileSize;
+          column = x / this.tileSize;
+          break;
+      }
+      const tile = this.gameMap[row][column];
+      // Legend:>> 0 - dots; 1 - wall; 2 - empty space; 3 - ghost; 4 - pacman; 5 - power pellet
+      // if tile === 1, we're colliding with a wall
+      if (tile === 1) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
